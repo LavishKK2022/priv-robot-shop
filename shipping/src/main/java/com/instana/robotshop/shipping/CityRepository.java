265950e53c262f2c6f5b3be9b7b9bedaaa82b16d
Lavish Kamal Kumar <115562651+LavishKK2022@users.noreply.github.com>
2025-04-23 13:19:11 +0100

    IBM Monitoring Service: UPDATE

--- shipping/src/main/java/com/instana/robotshop/shipping/CityRepository.java
+++ shipping/src/main/java/com/instana/robotshop/shipping/CityRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CityRepository extends CrudRepository<City, Long> {
-    List<City> findByCode(String code);
+    List<City> findByCode(String code)
 
     @Query(
         value = "select c from City c where c.code = ?1 and c.city like ?2%"
